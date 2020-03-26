38e1a35f01f67f2ecb689d1012c32efd61adfcf0
Emmanuel_Sosa <alcautersosa@gmail.com>
2020-03-25 19:39:26 -0600

    Proyecto de Personas en C#

---
+++ PersonaC#/Conexion2.cs
@@ -0,0 +1,49 @@
+﻿using Npgsql;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Persona
+{
+    
+    class Conexion2
+    {
+        private static string Host = "localhost:";
+        private static string User = "postgres";
+        private static string DBname = "aeropuerto";
+        private static string Password = "123456";
+        private static string Port = "5432";
+
+        static void Main(string[] args)
+        {
+
+            string connString =
+                String.Format(
+                    "Server={0};Username={1};Database={2};Port={3};Password={4};SSLMode=Prefer",
+                    Host,
+                    User,
+                    DBname,
+                    Port,
+                    Password);
+
+
+            using (var conn = new NpgsqlConnection(connString))
+
+            {
+                Console.Out.WriteLine("Abriendo Conexión");
+                conn.Open();
+
+                using (var command = new NpgsqlCommand("DROP TABLE IF EXISTS inventory", conn))
+                {
+                    command.ExecuteNonQuery();
+                    Console.Out.WriteLine("Finished dropping table (if existed)");
+
+                }
+
+                Console.WriteLine("Presiona Return para salir");
+                Console.ReadLine();
+
+            }
+        }
+    }
+}
---
+++ PersonaC#/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+
+namespace Persona
+{
+    public class Persona
+    {
+        public int IdPersona { get; set; }
+        public string Nombre { get; set; }
+        public string Apellidos { get; set; }
+        public string Curp { get; set; }
+        public string RFC { get; set; }
+        public string EdoCivil { get; set; }
+        public string Saludar()
+        {
+            return "Hola";
+        }
+    }
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+            Persona per = new Persona();
+        }
+
+    }
+}
